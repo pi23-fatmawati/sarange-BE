35c5511bf52261bd76f145862416ac3251245c57
llatifah99 <litalatifah6@gmail.com>
2024-02-09 18:53:55 +0700

    hope this is our last deploy please

--- models/user.js
+++ models/user.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      User.hasMany(models.Redeem, { foreignKey: "id_user" });
+      User.hasMany(models.Redeems, { foreignKey: "id_user" });
       User.hasMany(models.Transactions, { foreignKey: "id_user" });
     }
   }
